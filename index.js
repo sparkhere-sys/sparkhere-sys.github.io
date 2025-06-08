37c4aaae47cf9543c22b24461276a4d4282e213b
_Spark <abdelhalim5876@gmail.com>
2025-06-08 16:07:20 +0300

    index.js has some `console.log()`s so thats cool ig

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("god i hate javascript");
+console.log("i know im going to get an error because i forgot to add a semicolon...");
+console.log("also why are you even reading this? there's no easter eggs here buddy");
