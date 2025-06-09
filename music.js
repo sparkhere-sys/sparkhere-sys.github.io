7f539288998ccaf7e0561306db8ae319dd92b6d9
_Spark <abdelhalim5876@gmail.com>
2025-06-09 13:59:36 +0300

    added a cool looking 404 page

---
+++ music.js
@@ -0,0 +1,8 @@
+document.getElementById('song').addEventListener('click', function() {
+    let audio = new Audio("music.mp3");
+    audio.play();
+    console.log("le moozik is playing YAHOO");
+});
+
+// yep
+// toby fox please don't kill me for this
