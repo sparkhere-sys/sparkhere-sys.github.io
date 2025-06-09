86daf206d7d6322a87273946a29cce651743368e
_SparkHere <116731471+sparkhere-sys@users.noreply.github.com>
2025-06-09 14:04:16 +0300

    Update music.js

--- music.js
+++ music.js
@@ -1,8 +1,9 @@
+/*
 document.getElementById('song').addEventListener('click', function() {
     let audio = new Audio("assets/music.mp3");
     audio.play();
     console.log("le moozik is playing YAHOO");
 });
-
-// yep
-// toby fox please don't kill me for this
+*/
+// this chunk of code i commented out pretty much doesn't work and im too lazy to fix it
+console.log("Hello World!");
