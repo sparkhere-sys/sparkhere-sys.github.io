4a4d118da21e005cc6e7c3a332847517eccbbb46
_Spark <abdelhalim5876@gmail.com>
2025-06-10 11:38:16 +0300

    removed the thing

--- music.js
+++
@@ -1,9 +0,0 @@
-/*
-document.getElementById('song').addEventListener('click', function() {
-    let audio = new Audio("assets/music.mp3");
-    audio.play();
-    console.log("le moozik is playing YAHOO");
-});
-*/
-// this chunk of code i commented out pretty much doesn't work and im too lazy to fix it
-console.log("Hello World!");
