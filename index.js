7f00b1d8faf2cadc619504e716135ce23cfa20c9
_Spark <spark-aur@proton.me>
2026-04-06 16:45:35 +0300

    stuff

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-console.log("god i hate javascript");
-console.log("i know im going to get an error because i forgot to add a semicolon...");
-console.log("also why are you even reading this? there's no easter eggs here buddy");
+console.log("gotta (hate) love javascript");
