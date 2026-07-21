5fb75eac14e941210d2e42a1c85ed8cc48cf3677
_Spark <spark-aur@proton.me>
2026-07-21 12:33:37 +0200

    add redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  redirects: {
+    "/bolt": "https://discord.com/oauth2/authorize?client_id=1402185856806031390"
+  }
+});
